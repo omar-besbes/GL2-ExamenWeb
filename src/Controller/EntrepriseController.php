efbe972599f5cb8cd1d5ccada2ddf8f7338df44b
Omar Besbes <omar.besbes@insat.u-carthage.tn>
2022-05-05 17:07:28 +0100

    add form

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\EntrepriseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EntrepriseController extends AbstractController
 {
-    #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(): Response
-    {
-        return $this->render('entreprise/index.html.twig', [
-            'controller_name' => 'EntrepriseController',
-        ]);
-    }
+
+
+	public function __construct(private EntrepriseRepository $repository)
+	{
+	}
+
+	#[Route('/entreprise', name: 'app_entreprise')]
+	public function index(): Response
+	{
+		return $this->render('entreprise/index.html.twig', [
+			'controller_name' => 'EntrepriseController',
+			'entreprises' => $this->repository->findAll()
+		]);
+	}
 }
